bd388e8a2c1ccdea96fb929166034368c7902b51
Auston Bunsen <auston.bunsen@gmail.com>
2010-12-11 13:14:45 -0500

    fixed path issue (now useful in projects when dropped in as a folder)

--- __init__.py
+++ __init__.py
@@ -1,2 +1,8 @@
+#here so paython is recognized as a module
+import os, sys
+path = os.path.split(os.path.abspath(__file__))[0].split('/')
+path.pop()
+sys.path.append('/'.join(path))
+
 import gateways
 from lib import *
